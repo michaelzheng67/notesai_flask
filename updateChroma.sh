068205a9c4c1c3caf4f6f57afde097e795fcb5d6
michaelzheng67 <michaelzheng67@yahoo.ca>
2023-08-25 08:49:45 -0400

    feature: bash script to test cronjob

---
+++ updateChroma.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -H 'Content-Type: application/json' \
+      -d '{ "uid":"NmjO6WwswsbA5B8YTwFW8t1vzRw2"}' \
+      -X POST \
+     https://notesai-flask.onrender.com/create-chroma
