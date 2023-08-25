bd22645f666a1cbbbb16d763abeb119087d1a1ec
michaelzheng67 <michaelzheng67@yahoo.ca>
2023-08-25 09:13:29 -0400

    try running in background to reduce running time

--- updateChroma.sh
+++ updateChroma.sh
@@ -3,4 +3,4 @@
 curl -H 'Content-Type: application/json' \
       -d '{ "uid":"NmjO6WwswsbA5B8YTwFW8t1vzRw2"}' \
       -X POST \
-     https://notesai-flask.onrender.com/create-chroma
+     https://notesai-flask.onrender.com/create-chroma &>/dev/null &
